9b35fcb37b387eb74431bf1287bafbc6b6c862cc
ldolohov <collapsemetoo@gmail.com>
2021-07-30 18:12:14 +0900

    first working tests v0.1

--- __tests__/getdiff.test.js
+++ __tests__/getdiff.test.js
@@ -2,16 +2,9 @@
 import getDiff from '../lib/getdiff.js';
 import path from 'path';
 
-const testResult = `{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50 
-    + timeout: 20
-    + verbose: true
-  }
-  `;
+const testResult = `{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}`;
 
 test('basic functional', () => {
-    expect(getDiff('file1.json', 'file2.json')).toBe(testResult);
+    const expectResult = getDiff('file1.json', 'file2.json');
+    expect(expectResult).toMatch(testResult);
 })
