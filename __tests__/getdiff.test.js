87ffe35b2e9da341f9d4b790973107081f18a786
ldolohov <collapsemetoo@gmail.com>
2021-08-04 12:23:02 +0900

    Add fixes to go through lint rules

--- __tests__/getdiff.test.js
+++ __tests__/getdiff.test.js
@@ -1,10 +1,8 @@
-//import { test, expect } from '@jest/globals';
 import getDiff from '../lib/getdiff.js';
-import path from 'path';
 
-const testResult = `{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}`;
+const testResult = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 
 test('basic functional', () => {
-    const expectResult = getDiff('file1.json', 'file2.json');
-    expect(expectResult).toMatch(testResult);
-})
+  const expectResult = getDiff('file1.json', 'file2.json');
+  expect(expectResult).toMatch(testResult);
+});
