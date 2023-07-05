bd53e5db39ec1f0e27439f21cedc0cca178b9872
ldolohov <ldolohov@gmail.com>
2023-07-05 16:14:19 +0900

    correct linter mistakes

--- __tests__/getdiff.test.js
+++ __tests__/getdiff.test.js
@@ -1,8 +1,8 @@
 import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import genDiff from '../index.js';
 import { expect, test } from '@jest/globals';
 import fs from 'fs';
+import path, { dirname } from 'path';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
