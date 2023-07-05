3ac8106dbbf219b873232d43ccfd640dca25cbbc
ldolohov <ldolohov@gmail.com>
2023-07-05 11:14:08 +0900

    change gendiff file

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
-import program from 'commander';
+import { Command } from 'commander';
 import gendiff from '../index.js';
 
+const program = new Command();
+
 program
+  .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(gendiff(filepath1, filepath2, program.opts().format));
   });
